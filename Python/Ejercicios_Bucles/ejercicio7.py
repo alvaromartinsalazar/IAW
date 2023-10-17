ce725715b9aac1c63f5e5463e093afcc4e992332
CJ\amarsal <alvaromartinsalazar@gmail.com>
2023-10-17 10:53:25 +0200

    Commit ejercicio 7

--- Python/Ejercicios_Bucles/ejercicio7.py
+++ Python/Ejercicios_Bucles/ejercicio7.py
@@ -3,14 +3,14 @@
 #variable, pregunte al usuario por la contrase√±a hasta que introduzca la contrase√±a 
 #correcta.
 
-contra= "1234"
+contra= 1234
 contra_pedida=int(input("Ingresa una contraseña PIN: "))
-pin_correcto=True;
+pin_correcto=False;
 
 while (not pin_correcto):
-    if contra_pedida==contra:
-        print("Contraseña correcta, bienvenid@")
+    if contra==contra_pedida:
         pin_correcto=True
+        print("Contraseña correcta, bienvenid@")
     else:
         print("Contraseña incorrecta")
         contra_pedida=int(input("Ingresa una contraseña PIN: "))
