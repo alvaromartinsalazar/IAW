3dcf1f116a59afe86e3c56ed115916c90156403e
CJ\amarsal <alvaromartinsalazar@gmail.com>
2023-11-07 12:44:16 +0100

    Flask

---
+++ Flask/Prueba_1/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hola():
+    return render_template("index.html")
+
+@app.route("/eustaquio")
+def eustaquio():
+    return "Hola, esta es la web de eustaquio"
+
+@app.route("/pruebapost", methods=["POST", "GET"])
+def pruebapost():
+    if (request.method == "POST"):
+        return "Esto es un POST"
+    elif (request.method == "GET"):
+        return "Esto es un GET"
+    return "Hola, esta es la web de eustaquio"
+
+app.run()
