42bf1018a44d11c5f3431fa9c5ca391cd3c8faf0
DESKTOP-M0T6E0V\Alvaro <alvaro.martin.salazar@iesciudadjardin.com>
2023-11-13 18:37:54 +0100

    calculadora_imc

--- Flask/Ejercicio_IMC/app.py
+++ Flask/Ejercicio_IMC/app.py
@@ -1,15 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
-def imc():
-    peso=""
-    altura=""
-    c_imc=""
-    if (request.method == "POST"):
-        peso = float(request.form.get("peso"))
-        altura = float(request.form.get("altura"))
-    return render_template("index.html", c_imc=peso/altura*altura, peso=peso, altura=altura)
+def calcular_imc(altura, peso):
+    try:
+        altura = float(altura)
+        peso = float(peso)
+        imc = peso / (altura ** 2)
+        return imc
+    except ValueError:
+        return None
+
+def clasificar_imc(imc):
+    if imc is None:
+        return "Error en la entrada"
+    elif imc < 18.5:
+        return "Bajo peso"
+    elif 18.5 <= imc <= 24.9:
+        return "Normal"
+    elif 25.0 <= imc <= 29.9:
+        return "Sobrepeso"
+    else:
+        return "Obesidad"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        altura = request.form['altura']
+        peso = request.form['peso']
+        imc = calcular_imc(altura, peso)
+        clasificacion = clasificar_imc(imc)
+        return render_template('resultado.html', imc=imc, clasificacion=clasificacion)
+    return render_template('index.html')
 
 app.run()
