bd6a88c8d434054903e9a2757cecd6fc84ad3280
CJ\amarsal <alvaromartinsalazar@gmail.com>
2023-11-14 12:18:09 +0100

    calculadora imc

--- Flask/Ejercicio_IMC/app.py
+++ Flask/Ejercicio_IMC/app.py
@@ -3,13 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def calcular_imc(altura, peso):
-    try:
-        altura = float(altura)
-        peso = float(peso)
-        imc = peso / (altura ** 2)
-        return imc
-    except ValueError:
-        return None
+    altura = float(altura)
+    peso = float(peso)
+    imc = peso / (altura ** 2)
+    return imc
+
 
 def clasificar_imc(imc):
     if imc is None:
@@ -25,12 +23,18 @@ def clasificar_imc(imc):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        altura = request.form['altura']
-        peso = request.form['peso']
-        imc = calcular_imc(altura, peso)
-        clasificacion = clasificar_imc(imc)
-        return render_template('resultado.html', imc=imc, clasificacion=clasificacion)
-    return render_template('index.html')
+    try:
+        if request.method == 'POST':
+            altura = request.form['altura']
+            peso = request.form['peso']
+            imc = round(calcular_imc(altura, peso),2)
+            clasificacion = clasificar_imc(imc)
+            return render_template('resultado.html', imc=imc, clasificacion=clasificacion)
+        return render_template('index.html')
+    except ValueError:
+        return render_template('index.html', texto = "Introduce solo numeros")
+    except ZeroDivisionError:
+        return render_template('index.html', div0 = "No se puede dividir entre cero, introduce dos nuevos valores")
+        
 
 app.run()
